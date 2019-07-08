4daee6c3a0dc39de3ded33ce1aa8de2cb8ad55c4
Qparrot <parrot.quentin@gmail.com>
2019-07-08 18:58:16 +0200

    v1 ChoroplethMap Done

--- DataVisualizationProjects/VisualizeDataWithAChoroplethMap/index.js
+++ DataVisualizationProjects/VisualizeDataWithAChoroplethMap/index.js
@@ -1,6 +1,7 @@
 // Constant
 const w = 1000;
 const h = 500;
+const colors = ['#061412', '#0f322d', '#185049', '#216e64', '#2a8d80', '#33ab9d', '#37baa9', '#4ecbba', '#6cd3c6'];
 
 // Create the heading
 const heading = d3.select('body');
@@ -23,6 +24,7 @@ var svg = d3.select("body")
 var tooltip = d3.select('body')
 	.append('div')
 	.attr('id', 'tooltip')
+	.attr('class', 'tooltip')
 
 
 // Create the projection
@@ -38,10 +40,9 @@ Promise.all([worldmap, education]).then(function(values) {
 		.data(values[0].features)
 		.enter()
 		.append('path')
-		.attr('class', 'continent')
-		.attr('d', path)
-		// Add color scheme 
-		.style('fill', (d) => {
+		.attr('class', 'county')
+		.attr('data-fips', (d) => d.properties.STATE + d.properties.COUNTY)
+		.attr('data-education', (d) => {
 			var j = 0;
 			while (values[1][j])
 			{
@@ -50,60 +51,146 @@ Promise.all([worldmap, education]).then(function(values) {
 					name = values[1][j].fips.toString();
 				else
 					name = '0' + values[1][j].fips.toString();
+				if (name === '29203')
+					name === '46113';
 				if(d.properties.STATE + d.properties.COUNTY === name)
 				{
+					return(values[1][j].bachelorsOrHigher);
+				}
+				j++;
+			}
+		})
+		.attr('d', path)
+		// Add color scheme 
+		.style('fill', (d) => {
+			var j = 0;
+			while (values[1][j])
+			{
+				var fips = '';
+				
+				if(values[1][j].fips.toString().length === 5)
+					fips = values[1][j].fips.toString();
+				else
+					fips = '0' + values[1][j].fips.toString();
+				if(fips === '29203')
+					fips = '46113';
+				if(d.properties.STATE + d.properties.COUNTY === fips)
+				{
 					if(values[1][j].bachelorsOrHigher >= 66)
-						return('#061412');
+						return(colors[0]);
 					else if(values[1][j].bachelorsOrHigher >= 57)
-						return('#0f322d');
+						return(colors[1]);
 					else if(values[1][j].bachelorsOrHigher >= 48)
-						return('#185049');
+						return(colors[2]);
 					else if(values[1][j].bachelorsOrHigher >= 39)
-						return('#216e64');
+						return(colors[3]);
 					else if(values[1][j].bachelorsOrHigher >= 30)
-						return('#2a8d80');
+						return(colors[4]);
 					else if(values[1][j].bachelorsOrHigher >= 21)
-						return('#33ab9d');
+						return(colors[5]);
 					else if(values[1][j].bachelorsOrHigher >= 12)
-						return('#37baa9');
+						return(colors[6]);
 					else if(values[1][j].bachelorsOrHigher >= 3)
-						return('#4ecbba');
+						return(colors[7]);
 					else
-						return('6cd3c6');
+						return(colors[8]);
 				}
 				j++;
 			}
 		})
 
 		// On hover display the tooltip
-		.on('mousemove', (d) => {
+		.on('mouseover', (d) => {
 			tooltip.style('display', 'inline-block')
-			.html(d.properties.NAME);
+				.style('left', (d3.event.pageX + 10) + 'px')
+				.style('top', (d3.event.pageY + 10) + 'px')
+				.html(d.properties.NAME + ', State number:' + d.properties.STATE);
 		})
 		// out hover don't display the tooltip
 		.on('mouseout', (d) => {
 			tooltip.style('display', 'none')
+
 		});
-	// Points
-	
-	svg.selectAll('circle')
-		.data(values[1])
-		.enter()
-		.append('circle')
-		.attr('class', 'cities')
-		.attr('cx', (d) => projection([d.Longitude, d.Lattitude])[0])
-		.attr('cy', (d) => projection([d.Longitude, d.Lattitude])[1])
-		.attr('r', '2px')
-		.style('fill', '#f0f0f0');
-			
-	// labels
-	svg.selectAll('text')
-		.data(values[1])
-		.enter()
-		.append('text')
-		.text( (d) => d.City)
-		.attr('x', (d) => projection([d.Longitude, d.Lattitude])[0] + 5)
-		.attr('y', (d) => projection([d.Longitude, d.Lattitude])[1] + 15)
-		.attr('class', 'labels')
-		.style('fill', '#fff');
+
+	var g = svg.append("g")
+    .attr("class", "key")
+    .attr("id", "legend")
+		.attr("transform", "translate(0,40)");
+
+	g.append('rect')
+		.attr('x', w - 50)
+		.attr('y', h - 100)
+		.attr('height', 5)
+		.attr('width', 10)
+		.style('fill', colors[8])
+	g.append('rect')
+		.attr('x', w - 60)
+		.attr('y', h - 100)
+		.attr('height', 5)
+		.attr('width', 10)
+		.style('fill', colors[7]); 
+	g.append('rect')
+		.attr('x', w - 70)
+		.attr('y', h - 100)
+		.attr('height', 5)
+		.attr('width', 10)
+		.style('fill', colors[6]); 
+	g.append('rect')
+		.attr('x', w - 80)
+		.attr('y', h - 100)
+		.attr('height', 5)
+		.attr('width', 10)
+		.style('fill', colors[5])
+	g.append('rect')
+		.attr('x', w - 90)
+		.attr('y', h - 100)
+		.attr('height', 5)
+		.attr('width', 10)
+		.style('fill', colors[4]); 
+	g.append('rect')
+		.attr('x', w - 100)
+		.attr('y', h - 100)
+		.attr('height', 5)
+		.attr('width', 10)
+		.style('fill', colors[3]); 
+	g.append('rect')
+		.attr('x', w - 110)
+		.attr('y', h - 100)
+		.attr('height', 5)
+		.attr('width', 10)
+		.style('fill', colors[2])
+	g.append('rect')
+		.attr('x', w - 120)
+		.attr('y', h - 100)
+		.attr('height', 5)
+		.attr('width', 10)
+		.style('fill', colors[1]); 
+	g.append('rect')
+		.attr('x', w - 130)
+		.attr('y', h - 100)
+		.attr('height', 5)
+		.attr('width', 10)
+		.style('fill', colors[0]);
+	g.append('rect')
+		.attr('x', w - 130)
+		.attr('y', h - 102)
+		.attr('height', 10)
+		.attr('width', 2)
+		.style('fill', '#000');
+	g.append('rect')
+		.attr('x', w - 40)
+		.attr('y', h - 102)
+		.attr('height', 10)
+		.attr('width', 2)
+		.style('fill', '#000');
+	g.append('text')
+		.attr('x', w - 140)
+		.attr('y', h - 78)
+		.html('66%');
+	g.append('text')
+		.attr('x', w - 50)
+		.attr('y', h - 78)
+		.html('0%');
+
+		
 });
