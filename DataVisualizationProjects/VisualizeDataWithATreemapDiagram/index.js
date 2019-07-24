3b924d58752ca71fd1ad942154821d85a7a86f05
Qparrot <parrot.quentin@gmail.com>
2019-07-24 19:29:07 +0200

    V1 Done

--- DataVisualizationProjects/VisualizeDataWithATreemapDiagram/index.js
+++ DataVisualizationProjects/VisualizeDataWithATreemapDiagram/index.js
@@ -1,8 +1,10 @@
-const w = 500;
-const h = 500;
+const w = 960;
+const h = 550;
 const sales = d3.json('https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json');
+const padding = 1;
 let totalPerPlatform = {};
 let totalSales = 0;
+
 
 const heading = d3.select('body');
 	heading.append('h1')
@@ -19,29 +21,52 @@ const svg = d3.select('body')
 		.attr('height', h);
 
 Promise.all([sales]).then(function (results) {
-	svg.selectAll('rect')
-		.data(results[0].children)
-		.enter()
-		.append('rect')
-		.attr('width', 5)
-		.attr('height', 5)
-		.attr('x', w / 2)
-		.attr('y', h / 2)
-		.style('fill', 'red');;
 	
-	// Calculate the number of sales of all games.	
-	let i = 0;
-	while(results[0].children[i])
-	{
-		totalPerPlatform[results[0].children[i].name] = 0; 	
-		results[0].children[i].children.forEach(function(games) {
-			/*totalPerPlatform[results[0].children[i].name] */
-			totalPerPlatform[results[0].children[i].name] += parseInt(games.value);
-			totalSales += parseInt(games.value);
-		});
-		i++;
-	};
-	console.log(totalPerPlatform);
-	console.log(totalSales);
+	root = d3.hierarchy(results[0]);
+	root
+		.sum((d) => d.value)
+		.sort((a, b) => b.height - a.height || b.value - a.value);
 
+	var treemap = d3.treemap()
+		.size([w, h])
+		.padding(padding)
+		.round(true);
+
+	treemap(root);
+
+	var g = d3.select('svg')
+		.selectAll('.node')
+		.data(root.leaves())
+		.enter()
+		.append('g')
+		.attr('class', 'node')
+		.attr("transform",  (d) => "translate(" + d.x0 + "," + (d.y0) + ")");
+
+	g.append('rect')
+		.attr('width', (d) => d.x1 - d.x0)
+		.attr('height', (d) => d.y1 - d.y0)
+		.style('fill', (d) => 	{ 
+			while(d.depth > 1) {
+				d = d.parent;
+					return d3.schemeCategory10[parseInt(d.value % 7)];
+			}
+		})
+		.style('opacity', 0.6)
+
+	g.append('text')
+		.attr('x', 15)
+		.attr('dy', 4)
+		.style('text-decoration', 'underline')
+		.attr('font-size', '0.3em')
+		.text(d => 'On ' + d.data.category);
+
+	g.append('text')
+		.selectAll('tspan')
+		.data(d => d.data.name.split(/(?=[A-Z][^A-Z])/g))
+		.enter()
+		.append('tspan')
+		.attr('font-size', '0.3em')
+		.attr('x', 3)
+		.attr('y', (d, i) => 10 + 8*i)
+		.text(d => d);
 });
--- DataVisualizationProjects/VisualizeDataWithATreemapDiagram/readme.md
+++ DataVisualizationProjects/VisualizeDataWithATreemapDiagram/readme.md
@@ -1,14 +1,42 @@
 # VisualizeDataWithATreemapDiagram
 
+## Version
+
+- V1:
+	- For each game, I display a square with a size related to the amount of sale.
+	- For each game, I display the name of the game in the square with the amount of sales.
+	- It should be organised per platform.
+	- I don't take care of the color.
+	- I don't show the tooltip.
+	- The legend is not showed.
+
+- V2:
+	- Add the title headers. _(link 7)_
+	- each platform has a specific color.
+	- A tooltip is integrated.
 ## Log
 
 - (18.07.2019): commit 'first commit'
 	- Call the json file and show a square for each data.
+- (24.07.2019): commit 'add the function that calculate the total amound of sales for each pl…'
+	- Add a function that calculate the total of sales per platform and all platform together.
+- (24.07.2019): commit 'V1 done'
+	- Develop the V1 with the treemap of d3.
 
 ## Difficulties
+
+- (24.07.2019): 
+* How to do a carriage return?
+	- I need to warp the text in tspan see _(link 4)_
+* How to build a treemap in d5?
+	- I need to use the promise.then method. And use the d3.treemap. see _(link 5)_
 
 ## Useful link
 
 1. **Project page**: https://learn.freecodecamp.org/data-visualization/data-visualization-projects/visualize-data-with-a-treemap-diagram
 2. **D3 API Reference**: https://github.com/d3/d3/blob/master/API.md#axes-d3-axis
 3. **Data set used**: https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json
+4. **How to get d3 treemap cell text to wrap and not overflow other cells**: https://stackoverflow.com/questions/56623476/how-to-get-d3-treemap-cell-text-to-wrap-and-not-overflow-other-cells
+5. **D3.js v4/v5 treemap**: https://wizardace.com/d3-treemap/
+6. **Treemap template**: https://www.d3-graph-gallery.com/treemap
+7. **d3 Treemap with Title Headers**: http://billdwhite.com/wordpress/2012/12/16/d3-treemap-with-title-headers/
